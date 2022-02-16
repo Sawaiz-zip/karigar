9369f5162f36c3a626e8f41f7e95638a43c8dfac
lemon-sandwich <waseyu7119@gmail.com>
2022-02-16 20:43:46 +0500

    Softcoded padding in authentication

--- lib/screens/authentication.dart
+++ lib/screens/authentication.dart
@@ -8,137 +8,148 @@ class Authentication extends StatefulWidget {
 }
 
 class _AuthenticationState extends State<Authentication> {
-  TextEditingController _name = TextEditingController();
-  TextEditingController _email = TextEditingController();
-  TextEditingController _password = TextEditingController();
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
 
+  // ignore: non_constant_identifier_names
   final _name_validate = GlobalKey<FormState>();
+  // ignore: non_constant_identifier_names
   final _email_validate = GlobalKey<FormState>();
-
+  // ignore: non_constant_identifier_names
   final _password_validate = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+    final _size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                _size.width * 0.1, _size.height * 0.03, _size.width * 0.1, 0),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Text('Hire',
+                GestureDetector(
+                  onTap: () {},
+                  child: const Text('Hire',
                       style: TextStyle(
                         fontFamily: 'Poppins',
                         fontSize: 20,
                         color: Colors.black,
-                        decoration: TextDecoration.underline,
+                        //decoration: TextDecoration.underline
                       )),
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('Sell',
+                SizedBox(
+                  width: _size.width * 0.08,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: const Text('Sell',
                       style: TextStyle(
                         fontFamily: 'Poppins',
                         fontSize: 20,
                         color: Colors.black,
-                        decoration: TextDecoration.underline,
+                        //decoration: TextDecoration.underline
                       )),
                 )
               ],
             ),
-            SizedBox(
-              height: 100,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(50.0, 0, 0, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Hello,',
-                    style: TextStyle(
-                      fontFamily: 'GideonRoman',
-                      color: Colors.black,
-                      fontSize: 50,
-                    ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                _size.width * 0.1, _size.height * 0.1, _size.width * 0.2, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Hello,',
+                  style: TextStyle(
+                    fontFamily: 'GideonRoman',
+                    color: Colors.black,
+                    fontSize: 50,
                   ),
-                  Text(
-                    'create a new account',
-                    style: TextStyle(
-                      fontFamily: 'MoonTime',
-                      color: Colors.black,
-                      fontSize: 40,
-                    ),
+                ),
+                Text(
+                  'create a new account',
+                  style: TextStyle(
+                    fontFamily: 'MoonTime',
+                    color: Colors.black,
+                    fontSize: 40,
                   ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 50, 30),
-              child: Form(
-                key: _name_validate,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      hintText: 'Name',
-                      hintStyle: TextStyle(fontFamily: 'Poppins')),
-                  controller: _name,
-                  validator: (value) {
-                    if (value!.isEmpty || value == null)
-                      return 'Required Field';
-                    return null;
-                  },
                 ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                _size.width * 0.1, _size.height * 0.04, _size.width * 0.1, 0),
+            child: Form(
+              key: _name_validate,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    hintText: 'Name',
+                    hintStyle: TextStyle(fontFamily: 'Poppins')),
+                controller: _name,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Required Field';
+                  }
+                  return null;
+                },
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 50, 30),
-              child: Form(
-                key: _email_validate,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      hintText: 'Email',
-                      hintStyle: TextStyle(fontFamily: 'Poppins')),
-                  controller: _email,
-                  validator: (value) {
-                    if (value!.isEmpty || value == null)
-                      return 'Required Field';
-                    if (!value.contains("@") || !value.contains(".com"))
-                      return "Invalid Email!";
-                    return null;
-                  },
-                ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                _size.width * 0.1, _size.height * 0.04, _size.width * 0.1, 0),
+            child: Form(
+              key: _email_validate,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: TextStyle(fontFamily: 'Poppins')),
+                controller: _email,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Required Field';
+                  }
+                  if (!value.contains("@") || !value.contains(".com")) {
+                    return "Invalid Email!";
+                  }
+                  return null;
+                },
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 50, 30),
-              child: Form(
-                key: _password_validate,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: TextStyle(fontFamily: 'Poppins')),
-                  controller: _password,
-                  validator: (value) {
-                    if (value!.isEmpty || value == null)
-                      return 'Required Field';
-                    if (value.length < 5)
-                      return 'Password length must be greater than 5';
-                    return null;
-                  },
-                ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                _size.width * 0.1, _size.height * 0.04, _size.width * 0.1, 0),
+            child: Form(
+              key: _password_validate,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(fontFamily: 'Poppins')),
+                controller: _password,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Required Field';
+                  }
+                  if (value.length < 5) {
+                    return 'Password length must be greater than 5';
+                  }
+                  return null;
+                },
               ),
             ),
-            SizedBox(
-              height: 50,
-            ),
-            Center(
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                _size.width * 0.1, _size.height * 0.05, _size.width * 0.1, 0),
+            child: Center(
               child: SizedBox(
                 height: 45,
                 width: 200,
@@ -153,7 +164,7 @@ class _AuthenticationState extends State<Authentication> {
                             _email_validate.currentState!.validate() &&
                             _password_validate.currentState!.validate()) {}
                       },
-                      child: Center(
+                      child: const Center(
                           child: Text('Sign Up',
                               style: TextStyle(
                                 color: Colors.white,
@@ -163,10 +174,11 @@ class _AuthenticationState extends State<Authentication> {
                     )),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Center(
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                _size.width * 0.1, _size.height * 0.02, _size.width * 0.1, 0),
+            child: Center(
               child: SizedBox(
                 height: 45,
                 width: 200,
@@ -177,7 +189,7 @@ class _AuthenticationState extends State<Authentication> {
                     elevation: 7,
                     child: InkWell(
                       onTap: () async {},
-                      child: Center(
+                      child: const Center(
                           child: Text('Signup with Google',
                               style: TextStyle(
                                 color: Colors.black,
@@ -187,31 +199,32 @@ class _AuthenticationState extends State<Authentication> {
                     )),
               ),
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            Row(
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                _size.width * 0.1, _size.height * 0.18, _size.width * 0.1, 0),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
+                const Text(
                   'Already have an account?',
                   style: TextStyle(fontFamily: 'Poppins'),
                 ),
-                SizedBox(
+                const SizedBox(
                   width: 2,
                 ),
                 GestureDetector(
                   onTap: () {},
-                  child: Text(
+                  child: const Text(
                     'Log In',
-                    style: TextStyle(
+                    style: const TextStyle(
                         fontFamily: 'Poppins', fontWeight: FontWeight.bold),
                   ),
                 )
               ],
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
