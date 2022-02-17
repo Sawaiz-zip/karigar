5d933ea6542f3533b0e7ab221b554174ac8754e2
lemon-sandwich <waseyu7119@gmail.com>
2022-02-17 16:07:55 +0500

    Two button for option between screens

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Onboarding(),
+      home: MyHomePage(),
     );
   }
 }
@@ -29,12 +29,33 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-
-     floatingActionButton: FloatingActionButton(onPressed: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => Authentication()));
-      }),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => Authentication()));
+            },
+            child: Container(
+                color: Colors.grey,
+                height: 40,
+                width: 150,
+                child: Center(child: Text('Authentication Screen'))),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Onboarding()));
+            },
+            child: Container(
+                color: Colors.grey,
+                height: 40,
+                width: 150,
+                child: Center(child: Text('OnBoarding Screen'))),
+          ),
+        ],
+      ),
     );
   }
 }
